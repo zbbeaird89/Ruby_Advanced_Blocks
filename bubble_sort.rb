3fe6a8fb4ae158d74cc497eeb8fef0b895d11643
Zach Beaird <zbbeaird1989@gmail.com>
2016-09-10 21:51:07 -0500

    Solved bubble_sort_by / Refactored code

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -14,9 +14,8 @@ end
 
 def bubble_sort_by(array)
 	output = array
-	i = 0
 	until sorted? output
-		while i < output.length - 1
+		output.each_with_index do |num, i|
 			left, right = output[i], output[i + 1]
 			unless right == nil
 				value = yield left, right
@@ -24,13 +23,11 @@ def bubble_sort_by(array)
 					output[i] = right
 					output[i + 1] = left
 				end
-			end
-			i += 1
-		end
+			end 
+		end		
 	end
-	puts "#{output}"
+	output
 end
-
 
 def swap?(a, b)
 	b != nil && a > b
@@ -38,66 +35,16 @@ end
 
 def sorted? array
 	array.each_cons(2).all? do |a, b| 
-		unless b.nil?
+		if a.is_a?(Fixnum) && b.is_a?(Fixnum) && !b.nil?
+			a <= b
+		elsif a.is_a?(String) && b.is_a?(String) && !b.nil?
 			a.length <= b.length
 		end
 	end
 end
 
+bubble_sort([4, 3, 78, 2, 0, 2])
 
-#puts "#{bubble_sort([4, 3, 78, 2, 0, 2])}"
 bubble_sort_by(["hi", "hello", "hiya", "hey"]) do |left, right|  
 	left.length - right.length
 end
-
-
-
-# bubble_sort([4,3,78,2,0,2])
-# [0,2,2,3,4,78]
-
-#bubble_sort_by(["hi","hello","hey"]) do |left,right|
-   #left.length - right.length
-#end
-#["hi", "hey", "hello"]
-
-
-#[4, 3, 78, 2, 0, 2]
-#[3, 4, 78, 2, 0, 2]
-#[3, 4, 78, 2, 0, 2] 
-#[3, 4, 2, 78, 0, 2]
-#[3, 4, 2, 0, 78, 2]
-#[3, 4, 2, 0, 2, 78] 1st iteration push largest number to end
-
-#Complete 1st iteration
-
-#[3, 4, 2, 0, 2, 78]
-#[3, 4, 2, 0, 2, 78]
-#[3, 2, 4, 0, 2, 78]
-#[3, 2, 0, 4, 2, 78]
-#[3, 2, 0, 2, 4, 78]
-#[3, 2, 0, 2, 4, 78] 2nd iteration prepends second largest number to largest number
-
-#Complete 2nd iteration
-
-#[3, 2, 0, 2, 4, 78]
-#[2, 3, 0, 2, 4, 78]
-#[2, 0, 3, 2, 4, 78]
-#[2, 0, 2, 3, 4, 78]
-#[2, 0, 2, 3, 4, 78]
-#[2, 0, 2, 3, 4, 78] 3rd iteration prepends third largest number to 2nd largest number
-
-#Complete 3rd iteration
-
-#[2, 0, 2, 3, 4, 78]
-#[0, 2, 2, 3, 4, 78]
-#[0, 2, 2, 3, 4, 78]
-#[0, 2, 2, 3, 4, 78]
-#[0, 2, 2, 3, 4, 78]
-#[0, 2, 2, 3, 4, 78] etc.
-
-#Complete final iteration
-
-#WHAT IS THE STOPPING POINT FOR FINAL ITERATION
-
-
-
